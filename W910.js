7dd504e541478d3aa9d015739cd65a246d169d98
LostXofs <lostmmma@gmail.com>
2024-10-24 19:10:04 +0100

    Create W910.js

---
+++ W910.js
@@ -0,0 +1,44 @@
+
+function T1() {
+    location.href = "https://t.me/wmhcApI0gbc4MGI0";
+    vant.showNotify({
+        type: 'success',
+        message: 'Telegram'
+    });
+}
+function T2() {
+  location.href = "https://www.instagram.com/lostx_off?igsh=bXY1NmZ5dTJpYW16";
+  vant.showNotify({
+        type: 'success',
+        message: 'instgram'
+    });
+}
+function T3() {
+    location.href = "https://discord.gg/czGRV5YG";
+    vant.showNotify({
+        type: 'success',
+        message: 'Discord'
+    });
+}
+function T4() {
+  location.href = "https://t.me/+nrMeH94pkItmMGJk";
+  vant.showNotify({
+        type: 'success',
+        message: 'Whatsapp'
+    });
+}
+function T5() {
+    location.href = "https://fr.redtube.com/?search=cowgirl";
+    vant.showNotify({
+        type: 'success',
+        message: 'PORN HOP'
+    });
+}
+function T5() {
+    location.href = "https://fr.redtube.com/?search=cowgirl";
+    vant.showNotify({
+        type: 'success',
+        message: 'xnxx'
+    });
+}
+  
