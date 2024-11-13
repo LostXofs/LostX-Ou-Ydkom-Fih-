26e0b3a1802cea60739c21c4e05b136d6fb88824
LostXofs <lostmmma@gmail.com>
2024-11-13 17:50:02 +0100

    Create Wa9a.js

---
+++ Wa9a.js
@@ -0,0 +1,20 @@
+function XX8() {
+    h5gg.editAll('4366458311853685297', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: 'Speed X8'
+    });
+
+	}
+
+	
+	
+function XX5() {
+    h5gg.editAll('4366458311853685293', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: 'Speed X5'
+    });
+
+	}
+	
