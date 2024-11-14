50189af258c5a47a91602760169b32ffd1cf6611
LostXofs <lostmmma@gmail.com>
2024-11-14 23:35:54 +0100

    Update 10DAY.js

--- 10DAY.js
+++ 10DAY.js
@@ -1,12 +1,12 @@
 loopy() 
  function loopy() { 
  var sWord ="Tele : @LostXofs" 
- while (sWord != "DRAGON") {// Pass
+ while (sWord != "NEAM 01100100") {// Pass
  sWord = prompt("Enter The Key :")  
  } 
  
 //confirm("ok");
 
-window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ( DRAGON CREW )");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ");
 
  }
