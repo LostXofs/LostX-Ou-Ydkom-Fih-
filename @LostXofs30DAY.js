c2cd23291accc000db66ba45a2a29280a68f85c4
LostXofs <lostmmma@gmail.com>
2024-11-25 16:43:10 +0100

    Create @LostXofs30DAY.js

---
+++ @LostXofs30DAY.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "CarlosB12") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+   window.alert(" USE JB AND USE DNS ");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋  3 MONTH ");
+
+ }
