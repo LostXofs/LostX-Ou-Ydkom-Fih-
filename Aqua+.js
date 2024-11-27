72f834d3248fa36f3d109cabf1ae3e2ec8431df5
LostXofs <lostmmma@gmail.com>
2024-11-27 02:00:22 +0100

    Create Aqua+.js

---
+++ Aqua+.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "Carlos") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+   window.alert("USE DNS ");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 LostXAqua Non Jb ( 3 MONTH )");
+
+ }
