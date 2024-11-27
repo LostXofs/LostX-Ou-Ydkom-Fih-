e56518c4826dbb09fe76a55d17eb5490d4f8fe25
LostXofs <lostmmma@gmail.com>
2024-11-27 01:54:52 +0100

    Update KEY15.js

--- KEY15.js
+++ KEY15.js
@@ -1,12 +1,12 @@
 loopy() 
  function loopy() { 
  var sWord ="Tele : @LostXofs" 
- while (sWord != "Inar") {// Pass
+ while (sWord != "rvironvtbbiovfroennvn f") {// Pass
  sWord = prompt("Enter The Key :")  
  } 
  
 //confirm("ok");
 
-window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ( Inar )");
+window.alert("ERROR THE KEY IS END BUY NEW KEY @LostXofs");
 
  }
