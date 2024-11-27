5234a0f1e97fc8e60583e184198dc80dd559b9d8
LostXofs <lostmmma@gmail.com>
2024-11-27 01:57:33 +0100

    Update 15j.js

--- 15j.js
+++ 15j.js
@@ -1,12 +1,12 @@
 loopy() 
  function loopy() { 
  var sWord ="Tele : @LostXofs" 
- while (sWord != "Inar") {// Pass
+ while (sWord != "tgjithghit") {// Pass
  sWord = prompt("Enter The Key :")  
  } 
  
 //confirm("ok");
 
-window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ( Inar )");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ( ⨹ )");
 
  }
