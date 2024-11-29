01c26991b13bc5c5e59fa1bfcf920abf750aa0be
LostXofs <lostmmma@gmail.com>
2024-11-29 01:49:29 +0100

    Create DD1.js

---
+++ DD1.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "@Impooster") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+   window.alert("  ");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 IMPOOSTER");
+
+ }
