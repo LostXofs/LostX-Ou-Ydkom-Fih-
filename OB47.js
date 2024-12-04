1a7d8af963141fede29fccc5ec96071da37f0abd
LostXofs <lostmmma@gmail.com>
2024-12-04 20:29:23 +0100

    Create OB47.js

---
+++ OB47.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "LostXAqua") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+   window.alert(" NOTE : USE DNS FIRST ");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ");
+
+ }
