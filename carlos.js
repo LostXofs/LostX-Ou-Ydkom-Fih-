6d24884317b33605f829dd2e89bccb8fb3442706
LostXofs <lostmmma@gmail.com>
2024-12-06 00:50:14 +0100

    Create carlos.js

---
+++ carlos.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "carlosX") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ");
+
+ }
