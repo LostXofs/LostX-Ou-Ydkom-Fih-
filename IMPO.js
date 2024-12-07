191425943f7db283ce1cc530b71b7a709bd3f008
LostXofs <lostmmma@gmail.com>
2024-12-07 03:15:39 +0100

    Create IMPO.js

---
+++ IMPO.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "Impooster") {// Pass
+ sWord = prompt("𝗘𝗻𝘁𝗲𝗿 𝗧𝗵𝗲 𝗞𝗲𝘆 :")  
+ } 
+ 
+//confirm("ok");
+   window.alert("𝗧𝗵𝗶𝘀 𝗣𝗮𝗻𝗲𝗹  𝗶𝘀 𝗼𝗻𝗹𝘆 𝗮𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗳𝗼𝗿 𝗧𝗵𝗲 𝗔𝗱𝗺𝗶𝗻 𝗜𝗻 𝗖𝗹𝗮𝗻𝗲 𝗜𝗺𝗼𝘀𝘀𝘁𝗲𝗿");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝗜𝗠𝗣𝗢𝗦𝗦𝗧𝗘𝗥 𝗦𝗞𝗬  ");
+
+ }
