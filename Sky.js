3fc49a879a3c03a2b2b706bdc0c878f2f019a8c6
LostXofs <lostmmma@gmail.com>
2024-12-07 03:13:06 +0100

    Create Sky.js

---
+++ Sky.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "Impooster") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+   window.alert("𝗧𝗵𝗶𝘀 𝗣𝗮𝗻𝗲𝗹  𝗶𝘀 𝗼𝗻𝗹𝘆 𝗮𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗳𝗼𝗿 𝗧𝗵𝗲 𝗔𝗱𝗺𝗶𝗻 𝗜𝗻 𝗖𝗹𝗮𝗻𝗲 𝗜𝗺𝗼𝘀𝘀𝘁𝗲𝗿");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ");
+
+ }
