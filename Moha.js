f6736bae6eae3fc12e8491dc9cc04f3b9419e0ae
LostXofs <lostmmma@gmail.com>
2024-12-09 21:41:59 +0100

    Create Moha.js

---
+++ Moha.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "MohaXD") {// Pass
+ sWord = prompt("Enter The Key :")  
+ } 
+ 
+//confirm("ok");
+window.alert(" 𝗘𝘅𝗽𝗶𝗿𝗲𝘀 𝗟𝗶𝗳𝗲 𝗧𝗶𝗺𝗲 ( 𝗠𝗼𝗵𝗮 ) ");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝐀𝐪𝐮𝐚 ");
+
+ }
