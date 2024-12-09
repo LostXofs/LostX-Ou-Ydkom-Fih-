05625b2c073fccbc202f0769afc00e8093cfcd62
LostXofs <lostmmma@gmail.com>
2024-12-09 19:40:44 +0100

    Create Key001.js

---
+++ Key001.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Telegram : @LostXofs11" 
+ while (sWord != "@LostXofs") {// Pass
+ sWord = prompt("𝗘𝗻𝘁𝗲𝗿 𝗧𝗵𝗲 𝗞𝗲𝘆 ")  
+ } 
+ 
+//confirm("ok");
+window.alert(" † 𝗗𝗼𝗻𝘁 𝗦𝗵𝗲𝗮𝗿 𝗧𝗵𝗶𝘀 𝗣𝗮𝗻𝗲𝗹 𝗣𝗶𝘁𝗰𝗵 † ");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝗔𝗾𝘂𝗮 ( 𝗠𝗿 𝗠𝗼𝗵𝗮 )");
+ 
+ }
