b7b52218529a8508a7128511a34ef7f0f87bd8c7
LostXofs <lostmmma@gmail.com>
2024-12-25 22:37:49 +0100

    Create Alien.js

---
+++ Alien.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "LostX & Basic & Alien & Razi9a") {// Pass
+ sWord = prompt(" Enter The Key ;")  
+ } 
+ 
+//confirm("ok");
+   window.alert(" DOTN STIL THIS AW9");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ( Creart By @LostXofs )");
+
+ }
