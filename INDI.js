699ff4bee4b10e8d957cc19199428ff6034a85ab
LostXofs <lostmmma@gmail.com>
2025-02-09 00:18:45 +0100

    Create INDI.js

---
+++ INDI.js
@@ -0,0 +1,14 @@
+loopy() 
+ function loopy() { 
+ var sWord ="Tele : @LostXofs" 
+ while (sWord != "MASTER AYYA") {// Pass
+ sWord = prompt("Enter The Key ")  
+ } 
+ 
+//confirm("ok");
+   
+window.alert("Creadt By : @LostXofs");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 ");
+
+ }
